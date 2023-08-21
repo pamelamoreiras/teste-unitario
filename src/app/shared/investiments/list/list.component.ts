14420238a9b7931124281b526092025c0b9920c0
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-08-21 14:52:08 -0300

    Added investments list to banking component

--- src/app/shared/investiments/list/list.component.ts
+++ src/app/shared/investiments/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Model
+import { Investiments } from '../model/investiments';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public investiments: Array<Investiments> = [ 
+    {
+      name: "Itaú",
+      value: 100
+    },
+    {
+      name: "Banco do Brasil",
+      value: 100
+    },
+    {
+      name: "Nubank",
+      value: 100
+    },
+    {
+      name: "Inter",
+      value: 100
+    },
+  ]
 }
